738f017af2e0fb20b4b0a16a81479ed7f1679fa3
Yordan Petrov <105745677+ypetroff@users.noreply.github.com>
2022-08-03 23:25:45 +0300

    Add files via upload

---
+++ Programming Fundamentals/PF_DataTypesAndVariables_Lab/ConcatNames05.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class ConcatNames05 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String firstName = scanner.nextLine();
+        String lastName = scanner.nextLine();
+        String delimiter = scanner.nextLine();
+
+        System.out.printf("%s%s%s", firstName, delimiter, lastName);
+
+    }
+}
