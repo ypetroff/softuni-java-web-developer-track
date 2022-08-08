626a4ce45a68f40cee781e5cc0c1b46130ba5c25
Yordan Petrov <105745677+ypetroff@users.noreply.github.com>
2022-08-08 23:18:36 +0300

    Add files via upload

---
+++ Programming Fundamentals/PF_DataTypesAndVariables-MoreExercise/RefactoringPrimeChecker04.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class RefactoringPrimeChecker04 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int number = Integer.parseInt(scanner.nextLine());
+        for (int i = 2; i <= number; i++) {
+            boolean isPrime = true;
+            for (int divider = 2; divider < i; divider++) {
+                if (i % divider == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+            System.out.printf("%d -> %b%n", i, isPrime);
+
+        }
+    }
+}
